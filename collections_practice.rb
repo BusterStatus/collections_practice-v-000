dab5e4a14ccfa88e8f50db54ee0575d226326136
matthew.serre <matthew.serre@gmail.com>
2018-10-29 01:00:34 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  new_three = array[1]
+  new_two = array[2]
+  array[1] = new_two
+  array[2] = new_three
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each {|x|
+    subarray = x.split
+    subarray[2] = "$"
+    subarray.to_s
+    new_array << subarray
+  }
+  new_array
+end
